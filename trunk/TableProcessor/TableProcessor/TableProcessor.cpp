6eaf3bb58f8fc322ef8ba4f7d9ca1c6461b0f948
plastiv <plastiv@fb4b1917-370a-0c84-de19-b6e0e16a7844>
2011-11-09 14:15:46 +0000

    Little bit refactor on style.

--- trunk/TableProcessor/TableProcessor/TableProcessor.cpp
+++ trunk/TableProcessor/TableProcessor/TableProcessor.cpp
@@ -10,9 +10,6 @@
 #include <sstream>
 #include <stdexcept>
 
-using std::cin;
-using std::cout;
-
 // Error message function for TableProcessor
 void error(const char* why, const char* what="")
 {
@@ -26,32 +23,22 @@ int main(int argc, char* argv[])
 	// Expected 2 parameters inputFileName.txt outputFileName.txt
 	if (argc != 3 ) error ("wrong number of arguments");
 
-	cout << "Read table from file "  << argv[1]<< endl;
+	std::cout << "Read table from file "  << argv[1]<< endl;
 	std::ifstream inputFile(argv[1]);	//open input file stream
 	if (!inputFile) error ("cannot open input file", argv[1]);
 
 	Table table;
 	inputFile >> table;
-	cout << table << endl;
+	std::cout << table << endl;
 
-	try
-	{
-		table.processAllCells();
-	}
-	catch (std::out_of_range& oor)
-	{
-		error("Out of Range error: ", oor.what() );
-	}
+	table.processAllCells();
 
-	cout << "Processed table" << endl << table << endl;
-	cout << "Write table to file " << argv[2] << endl;
+	std::cout << "Processed table" << endl << table << endl;
+	std::cout << "Write table to file " << argv[2] << endl;
 
 	std::ofstream outputFile(argv[2]);	//open output file stream
 	if (!outputFile) error ("cannot open output file", argv[2]);
 	outputFile << table;
 
-	//int wait;
-	//cin >> wait;
 	return 0;
 }
-
